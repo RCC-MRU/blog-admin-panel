90d2f9104fa2e3aaa06a59383bd754b0fb22c31c
Arsh <arshlakhina2002@gmail.com>
2021-06-10 22:49:00 +0530

    Converted to react but not responsive

--- client/src/Components/Comments.jsx
+++ client/src/Components/Comments.jsx
@@ -1,7 +1,62 @@
 import React from "react";
 
 const Comments = () => {
-  return <h1 className="text-info">Comments</h1>;
-};
+  return(
+  <React.Fragment>
+  <h1 className="text-info">Comments</h1>;
+  <div className="container">
+      <div class="top">
+          <h1>Comments</h1>
+          <p>
+              All<span>(80)</span>|Highlighted<span>(25)</span>|Liked<span>(10)</span>
+          </p>
+      </div>
+      <hr/>
+      <div className="bottom">
+          <table style="width: 100%">
+              <tr>
+                  <th>Author</th>
+                  <th>Comment</th>
+              </tr>
+              <tr>
+                  <td>
+                      <ul>
+                          <li>Author</li>
+                      </ul>
+                      author@gmail.com
+                      <br/>
+                      198.5468.24648.378
+                  </td>
+                  <td>
+                      Hii ,
+                      <br/>
+                      comment written by author.
+                      <br/><br/>
+                      Reply | ignore | unapproven | Spam | Trash
+                  </td>
+              </tr>
+              <tr>
+                  <td>
+                      <ul>
+                          <li>Author</li>
+                      </ul>
+                      author@gmail.com
+                      <br/>
+                      198.5468.24648.378
+                  </td>
+                  <td>
+                      Hii ,
+                      <br/>
+                      comment written by author.
+                      <br/><br/>
+                      Reply | ignore | unapproven | Spam | Trash
+                  </td>
+              </tr>
+          </table>
+      </div>
+  </div>
+  </React.Fragment>
+  );
+}
 
 export default Comments;
