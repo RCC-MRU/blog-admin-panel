c6abfe0ac0297846d445b0792c72695c1f32961c
Sanchit <sanchit02@outlook.com>
2021-07-30 21:15:10 +0530

    added login and posts

--- client/src/Components/Posts.jsx
+++ client/src/Components/Posts.jsx
@@ -1,21 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Table } from "reactstrap";
 import axios from "axios";
+import { BlogContext } from "../Context/BlogContext";
+// import { showAuthorPost } from "../Util/axios";
+
+// const num = 1;
 
 const Posts = () => {
-
-  const [posts, setPosts] = useState({ blogs: [] });
+  const [posts, setPosts] = useState([]);
+  const context = useContext(BlogContext);
 
   useEffect(() => {
-    const fetchPostList = async () => {
-      const { data } = await axios(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      setPosts({ blogs: data });
-      console.log(data);
-    };
-    fetchPostList();
-  }, [setPosts]);
+    axios({
+      url: "http://localhost:3003/posts",
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${context.user?.token}`,
+      },
+    }).then((data) => {
+      setPosts(data.data.result);
+      console.log(data.data.result);
+    });
+  }, [context.user?.token]);
 
   return (
     <React.Fragment>
@@ -40,20 +46,27 @@ const Posts = () => {
               </tr>
             </thead>
             <tbody>
-
-
-              {posts.blogs &&
-            posts.blogs.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.title}</td>
-                <td>{item.body}</td>
-                <td>{item.title}</td>
-                <td>{item.title}</td>
-              </tr>
-            ))}
-
-
+              {/* {posts.blogs &&
+                posts.blogs.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.title}</td>
+                    <td>{item.body}</td>
+                    <td>{item.title}</td>
+                    <td>{item.title}</td>
+                  </tr>
+                ))} */}
+              {posts.map((item, i) => {
+                return (
+                  <tr key={item.blogId}>
+                    <td>{i + 1}</td>
+                    <td>{item.blogTitle}</td>
+                    <td>{item.userId}</td>
+                    <td>{item.category}</td>
+                    <td>{new Date(item.createdAt).toLocaleDateString()}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </Table>
         </section>
