7afe143527ef1dcf02996bfeec13589817e70137
Sanchit <sanchit02@outlook.com>
2021-08-10 00:45:22 +0530

    delete post and delete comments working

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -4,7 +4,7 @@ const db = require("../database/db");
 //show Author Posts
 module.exports = {
   showAuthorPost: async function (req, res) {
-    let sql = `Select * FROM blog WHERE userId ='${req.result.userId}'`;
+    let sql = `Select * FROM blog WHERE userId ='${req.result.userId}' AND visibility = '1'`;
 
     const query = db.query(sql, (err, result) => {
       if (err) throw err;
@@ -16,14 +16,13 @@ module.exports = {
     console.log(query.sql);
   },
 
-  
-
   //show comments on author post
   showPostComments: async function (req, res) {
-    let sql = `SELECT * FROM comments Where userId ='${req.result.userId}'`;
+    let sql = `SELECT C.commentId, C.author, B.blogTitle, C.comment, C.createdAt FROM blog B INNER JOIN comments C WHERE B.blogId = C.blogId AND C.userId='${req.result.userId}' AND C.visibility='1'`;
 
     const query = db.query(sql, (err, result) => {
       if (err) throw err;
+
       res.status(200).json({
         message: "Comments on post are displayed",
         result: result,
@@ -32,10 +31,8 @@ module.exports = {
     console.log(query.sql);
   },
 
-
-
   //Create new post
-  NewPost: async function (req, res) {
+  newPost: async function (req, res) {
     let CreatePost = req.body;
     let sql = `INSERT INTO blog SET ?`;
 
@@ -47,35 +44,35 @@ module.exports = {
       });
     });
     console.log(query.sql);
-},
+  },
 
-
- // Delete the post
-  DeletePost: async function (req,res) {
- let sql= `DELETE FROM blog WHERE blogId ='${req.params.blogId}'`;
-
- const query = db.query(sql, (err, result) => {
- if (err) throw err;
- res.status(200).json({
-   message: "Post is deleted",
-   result: result,
-  });
-});
-console.log(query.sql);
-},
-
-
-
-//  Show Blog Title 
-  BlogTitle: async function (req,res) {
-    let sql= ` SELECT blogTitle FROM blog WHERE blogId ='${req.params.blogId}'`;
+  // Delete the post
+  deletePost: async function (req, res) {
+    // let sql = `DELETE FROM blog WHERE blogId ='${req.params.blogId}'`;
+    let sql = `UPDATE blog SET visibility = 0 WHERE blogId='${req.params.blogId}'`;
 
     const query = db.query(sql, (err, result) => {
       if (err) throw err;
       res.status(200).json({
+        message: "Post deleted",
         result: result,
-       });
-     });
-     console.log(query.sql);
-     },
+      });
+    });
+    console.log(query.sql);
+  },
+
+  // Delete the post
+  deleteComment: async function (req, res) {
+    // let sql = `DELETE FROM blog WHERE blogId ='${req.params.blogId}'`;
+    let sql = `UPDATE comments SET visibility = 0 WHERE commentId='${req.params.commentId}'`;
+
+    const query = db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.status(200).json({
+        message: "Comment deleted",
+        result: result,
+      });
+    });
+    console.log(query.sql);
+  },
 };
